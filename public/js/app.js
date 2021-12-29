a20ca803abe3b390fa7d5440ae6299ab724b3503
suzon-ahmmed <info.suzonahamed@gmail.com>
2021-12-29 19:32:22 +0600

    parent to child and child to parent

--- public/js/app.js
+++ public/js/app.js
@@ -86,9 +86,115 @@
 
 
 // console.log(allLaastChild);
-let grandparent = document.querySelector('.nav-list');
-let parent = grandparent.children;
-let chield = parent[0];
-console.log(chield);
+// let grandparent = document.querySelector('.nav-list');
+// let parent = grandparent.children;
+// let chield = parent[0];
+// console.log(chield);
+
+/*
+ * Title: To Do Application using vanilla JS DOM
+ * Description: This JS file has all the JS functions necessary to control the to do application
+ * Author: Sumit Saha ( Learn with Sumit )
+ * Date: 12/17/2020
+ *
+ */
+
+//  // select elements & assign them to variables
+//  let newTask = document.querySelector('#new-task');
+//  let form = document.querySelector('form');
+//  let todoUl = document.querySelector('#items');
+//  let completeUl = document.querySelector('.complete-list ul');
+
+
+// // functions
+// let createTask = function(task) {
+//     let listItem = document.createElement('li');
+//     let checkBox = document.createElement('input');
+//     let label = document.createElement('label');
+
+//     label.innerText = task;
+//     checkBox.type = 'checkbox';
+
+//     listItem.appendChild(checkBox);
+//     listItem.appendChild(label);
+
+//     return listItem;
+// }
+
+// let addTask = function(event) {
+//     event.preventDefault();
+//     let listItem = createTask(newTask.value);
+//     todoUl.appendChild(listItem);
+//     newTask.value = "";
+//     // bind the new list item to the incomplete list
+//     bindInCompleteItems(listItem, completeTask);
+// }
+
+// let completeTask = function() {
+//     let listItem = this.parentNode;
+//     let deleteBtn = document.createElement('button');
+//     deleteBtn.innerText = 'Delete';
+//     deleteBtn.className = 'delete';
+//     listItem.appendChild(deleteBtn);
+
+//     let checkBox = listItem.querySelector('input[type="checkbox"]');
+//     checkBox.remove();
+//     completeUl.appendChild(listItem);
+//     bindCompleteItems(listItem, deleteTask);
+// }
+
+// let deleteTask = function() {
+//     let listItem = this.parentNode;
+//     let ul = listItem.parentNode;
+//     ul.removeChild(listItem);
+// }
+
+// let bindInCompleteItems = function(taskItem, checkboxClick) {
+//     let checkBox = taskItem.querySelector('input[type="checkbox"]');
+//     checkBox.onchange = checkboxClick;
+// }
+
+// let bindCompleteItems = function(taskItem, deleteButtonClick) {
+//     let deleteButton = taskItem.querySelector('.delete');
+//     deleteButton.onclick = deleteButtonClick;
+// }
+
+// for(let i=0; i< todoUl.children.length; i++ ) {
+//     bindInCompleteItems(todoUl.children[i], completeTask);
+// }
+
+// for(let i=0; i< completeUl.children.length; i++ ) {
+//     bindCompleteItems(completeUl.children[i], deleteTask);
+// }
+
+// form.addEventListener('submit', addTask);
+
+/*
+ * Title: To Do Application using vanilla JS DOM
+ * Description: This JS file has all the JS functions necessary to control the to do application
+ * Author: Sumit Saha ( Learn with Sumit )
+ * Date: 12/17/2020
+ *
+ */
+// let grandparent = document.querySelector('.todo-list')
+// let parent = grandparent.children;
+// let children = parent[1].children;
+// console.log(children); 
+
+//direct children
+// let grandparent = document.querySelector('.todo-list')
+
+// let children = grandparent.querySelectorAll('.item');
+// console.log(children);
+
+
+// //children to parent
+// let children = document.querySelector('.item');
+// let parent = children.parentElement;
+// console.log(parent);
+//children to direct grandparent
+const children = document.querySelector('.item');
+const grandparent = children.closest('.todo-list');
+console.log(grandparent);
 
 
