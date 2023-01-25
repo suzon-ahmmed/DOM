8acb1662644b59a912b3128d0a39f3d6ce7eaa4d
suzon-ahmmed <info.suzonahamed@gmail.com>
2023-01-25 22:57:24 +0600

    add some demo page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {
-     
+      colors: {
+        demoColor: "#489fd9",
+        //dark background for code block
+        "primary-header": "#741ce8",
+        //socil media icon
+        github: "#171515",
+        linkedin: "#1d66c2",
+        facebook: "#2677f2",
+        twiter: "#2ba1f2",
+        behance: "#2557ff",
+        youtube: "#FF0000",
+      },
     },
   },
   container: {
